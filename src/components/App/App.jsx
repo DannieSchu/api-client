671cd0304aab296b55e5e85c4d84a8350420b8fd
Dannie Schumaker <draeschumaker@gmail.com>
2020-04-30 17:17:34 -0700

    create Header component

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import Form from '../Form/Form';
+import Header from '../Header/Header';
 
 export default function App() {
-  return <Form 
-    onSubmit={() => {}} 
-    onJsonChange={() => {}} 
-    onUrlChange={() => {}} 
-    onMethodChange={() => {}} 
-    url="https://history.muffinlabs.com/date" 
-    method="get" 
-    buttonText="Send" />;
+  return (
+    <>
+      <Header />
+      <Form
+        onSubmit={() => { }}
+        onJsonChange={() => { }}
+        onUrlChange={() => { }}
+        onMethodChange={() => { }}
+        url="https://history.muffinlabs.com/date"
+        method="get"
+        buttonText="Send" />;
+    </>);
 }
